69a9eb46c311a159f51cf5f14bbbb9a5512a123b
Isaac Bigsby Trogdon <mr@dyss.net>
2017-10-13 15:52:22 -0400

    Playback and controller complete.

--- static/js.js
+++ static/js.js
@@ -5,7 +5,9 @@
  *  3. css wrapper id for scene elements
  *  4. css wrapper id for video info
  */
-var player = videoPlayer('/episode-test/data.json', 'video-player', 'scene-elements', 'video-info');
+var player = null; // set in function
+var d = null; // set in function
+var videoPlayer = videoPlayer('/episode-test/data.json', 'video-player', 'scene-elements', 'video-info');
 var timeOuts = [];
 
 /**
@@ -16,13 +18,13 @@ var timeOuts = [];
  * @param {*} infoId
  */
 function videoPlayer(jsonPath, playerId, elementsId, infoId) {
-// $.playerInit = function(jsonPath, playerId, elementsId, infoId) {
   // Load json data.
   $.getJSON(jsonPath, { get_param: 'value' }, function(data) {
-    var d = data;
+    d = data;
     d.playerId = playerId;
     d.elementsId = elementsId;
     d.infoId = infoId;
+    d.playthrough = true;
 
     // Set scene indices.
     sElementsIndexing(d);
@@ -41,7 +43,7 @@ function videoPlayer(jsonPath, playerId, elementsId, infoId) {
     };
 
     // Initialize main player.
-    var player = new Vimeo.Player(d.playerId, options);
+    player = new Vimeo.Player(d.playerId, options);
 
     // Dev settings.
     if (d.dev == true) {
@@ -62,27 +64,6 @@ function videoPlayer(jsonPath, playerId, elementsId, infoId) {
       });
     });
 
-    // Event listeners.
-    // 'play'
-    player.on('play', function(data) {
-      player.getCurrentTime().then(function(curTime) {
-        // Playback sequence
-        playbackSequence(d, curTime);
-      });
-    });
-
-    // 'pause'
-    player.on('pause', function(data){
-      $.each(timeOuts, function(i, e){
-        // clearTimeout(e);
-      });
-    });
-    // 'cuepoint'
-    player.on('cuepoint', function(data){
-      console.log(data.data.id + ' ' + data.data.action + ' at ' + data.time );
-      $('#' + data.data.id).toggleClass('active');
-    });
-
   });
 }
 
@@ -92,9 +73,9 @@ function videoPlayer(jsonPath, playerId, elementsId, infoId) {
  * @param {*} curTime
  */
 function playbackSequence(d, curTime) {
+  console.log('playback sequence function');
   hideElements(d.sceneElements);
-  var sEcur  = getCurrentSE(d, curTime);
-  showElements(sEcur);
+  showElements(getCurrentSE(d, curTime));
 }
 
 /**
@@ -186,36 +167,194 @@ function placeSceneElementDevInfo(d) {
       + '</tr>';
 
       $.each(d.sceneElements, function(i, e) {
-        var radioIn = '<input type="radio" name="scene-selector" value="';
+        var radioIn = '<input type="radio" name="scene-selector" target="scene" value="';
         var checked = (i == 0) ? 'checked="checked"' : null;
         var radioOut = '" ' + checked + '>';
+        var file = '/' + d.episodeId + '/files/' + e.fileName;
+        var attrClasses = (i == 0) ? ' class="active scene"' : ' class="scene"';
         markup
-        += '<tr>'
+        += '<tr' + attrClasses + ' id="' + e.id + '">'
           + '<td>' + radioIn + e.id + radioOut + e.id + '</td>'
-          + '<td>' + e.position.x + '</td>'
-          + '<td>' + e.position.y + '</td>'
+          + '<td id="posX">' + e.position.x + '</td>'
+          + '<td id="posY">' + e.position.y + '</td>'
           + '<td>' + e.time.in + '</td>'
           + '<td>' + e.time.out + '</td>'
+          + '<td><img src="' + file + '"></td>'
         + '</tr>';
       });
+      // Add playback button & info.
+      markup
+      += '<tr id="actions">'
+        + '<td id="play-scene"><a id="play"class="playback active">Play</a><a id="pause" class="playback playing">Pause</a></td>'
+        + '<td id="posX" class="calc"><a id="plus" value=1 target="left">+</a><a id="minus" value=-1 target="left">—</a></td>'
+        + '<td id="posY" class="calc"><a id="plus" value=1 target="top">+</a><a id="minus" value=-1 target="top">—</a></td>'
+        + '<td class="time"><a id="time-in" class="time">←</a></td>'
+        + '<td class="time"><a id="time-out" class="time">→</a></td>'
+      + '</tr>';
       // Edit info.
       markup
       += '<tr id="edits">'
         + '<td id="editId">Edits</td>'
-        + '<td id="posX"></td>'
-        + '<td id="posY"></td>'
-        + '<td id="timeIn"></td>'
-        + '<td id="timeOut"></td>'
-      + '</tr>';
-      // Add playback button & info.
-      markup
-      += '<tr id="actions">'
-        + '<td id="play-scene"><a>Play</a></td>'
-        + '<td id="posX" class="calc"><a id="plus">+</a><a id="minus">—</a></td>'
-        + '<td id="posY" class="calc"><a id="plus">+</a><a id="minus">—</a></td>'
+        + '<td id="posX"><button id=posXbutton class="btn" data-clipboard-target="#posXbutton"></td>'
+        + '<td id="posY"><button id=posYbutton class="btn" data-clipboard-target="#posYbutton"></td>'
       + '</tr>';
       // close markup
       markup += '</tr></table></form>';
     $('#' + d.infoId).append(markup);
   }
 }
+
+/**
+ * Scene editor
+ */
+$(document).ready(function(){
+  player.play()
+  player.pause();
+  var playButton = $('#scene-edit-form #play-scene a');
+  // Event listeners.
+  // 'play'
+  player.on('play', function(data) {
+    player.getCurrentTime().then(function(curTime) {
+      // Playback sequence
+      playbackSequence(d, curTime);
+    });
+  });
+
+  // 'pause'
+  player.on('pause', function(data){
+    // pause
+  });
+  // 'cuepoint'
+  player.on('cuepoint', function(data){
+    console.log(data.data.id + ' ' + data.data.action + ' at ' + data.time );
+    switch(data.data.action) {
+      case 'showElement':
+          $('#' + data.data.id).addClass('active');
+          if (d.playthrough == true)
+            editFormSwitchActiveRow($('.scene#' + data.data.id));
+          break;
+      case 'hideElement':
+        $('#' + data.data.id).removeClass('active');
+        // if (d.single == true)
+        if (
+          d.single.length > 0
+          && d.single == data.data.id
+        ){
+          player.pause();
+          playButton.toggleClass('active');
+        }
+        break;
+    }
+  });
+
+  // Editor controls.
+  var form = $('#scene-edit-form');
+  function editFormSwitchActiveRow(target) {
+    $.each(target.siblings(), function(i, e) {
+      $(e).removeClass('active');
+    });
+    target.addClass('active');
+    target.find('input').attr('checked', 'checked');
+    $('#edits #posX').html(target.find('#posX').html())
+    $('#edits #posX').attr('value', target.find('#posX').html())
+    $('#edits #posY').html(target.find('#posY').html())
+    $('#edits #posY').attr('value', target.find('#posY').html())
+  }
+  // switch active rows on click.
+  form.find('input[name="scene-selector"]').click(function(){
+    var target = $(this).parents('.' + $(this).attr('target'));
+    editFormSwitchActiveRow(target);
+  });
+  //prevent links default
+  form.find('a').each(function(){
+    $(this).click(function(event){
+      event.preventDefault();
+    });
+  });
+
+  // Play single scene.
+  $('#scene-edit-form #play-scene a#play').click(function(event){
+    var activeId = form.find('tr.active input[checked="checked"]').attr('value');
+    console.log(activeId);
+    d.single = activeId;
+    d.playthrough = false;
+    player.getCuePoints().then(function(cuePoints) {
+      $.each(cuePoints, function(i, e) {
+        if (
+          e.data.action == 'showElement'
+          && e.data.id == activeId
+        ) {
+          player.setCurrentTime(e.time);
+          player.play();
+          playbackSequence(d, e.time);
+        }
+      });
+    });
+  });
+
+  // Set to scene point.
+  $('#scene-edit-form a.time').click(function(event){
+    var link = $(this);
+    var activeId = form.find('tr.active input[checked="checked"]').attr('value');
+    $.each(d.sceneElements, function(i, e){
+      if (e.id == activeId) {
+        var curTime;
+        if (link.attr('id') == 'time-in')
+          curTime = e.time.in;
+        if (link.attr('id') == 'time-out')
+          curTime = e.time.out;
+        player.setCurrentTime(curTime);
+      }
+    });
+  });
+
+  // Edit scene element position.
+  $('#scene-edit-form td.calc a').click(function(event){
+    console.log('click')
+    var link = $(this);
+    var activeId = form.find('tr.active input[checked="checked"]').attr('value');
+    var position = link.attr('target');
+    var id = link.parent().attr('id');
+    var target = $('tr#' + activeId + ' td#' + id);
+    var editCell = $('#edits ' + '#' + id);
+    var attr = editCell.attr('value');
+    if (typeof attr !== typeof undefined && attr !== false) {
+        // ...
+    }
+    var targetValue = parseInt(editCell.attr('value'));
+    // var targetValue = (typeof attr !== typeof undefined && attr !== false)
+      // ? parseInt(editCell.attr('value')) : parseInt(target.html());
+    var value = parseInt(link.attr('value'));
+    var newValue = (targetValue + value);
+    $('#edits').addClass('active');
+    $('#edits ' + '#' + id).html(targetValue + value).attr('value', newValue)
+    console.log(position + ' ' + newValue + 'px');
+    var sE = $('.scene-element#' + activeId);
+    var asdf = 1;
+    if (position == 'top') {
+      sE.css({
+        'top' : newValue + 'px'
+      });
+    }
+    if (position == 'left') {
+      sE.css({
+        'left' : newValue + 'px'
+      });
+    }
+  });
+
+  /**
+   * pause button.
+   */
+  $('#scene-edit-form #play-scene a#pause').click(function(event){
+    player.pause();
+  });
+
+  /**
+   * toggle active class
+   */
+  playButton.click(function(){
+    $(this).parent().children('a').toggleClass('active');
+  });
+
+});
