64c4b2275b59935ed1676f66a0206edee508e28d
Isaac Bigsby Trogdon <mr@dyss.net>
2017-12-17 17:02:14 +0100

    Bounce in wall animation update.

--- static/js/thumbnails.js
+++ static/js/thumbnails.js
@@ -2,40 +2,24 @@
  * Scene elements - thumbnails & videos.
  */
 $(document).ready(function(){
-  var $wr = $('#scene-elements');
-
-  // Init names from data and insert into wrapper.
-  // Fade in names.
-  initTextEl(d.n, $wr);
-
-  // if (condition == 'true'){
-  //   function1(someVariable, function() {
-  //     function2(someOtherVariable);
-  //   });
-  // }
-  // else {
-  //     doThis(someVariable);
-  // }
-
-
-  sPreEtransition();
-  sEtransition();
-  // sceneElementsFadeTransition();
-
-  // Init titles from data and insert into wrapper.
-  // Fade in + images with zoom effect.
-  initTextEl(d.e, $wr);
-
   $('body').imagesLoaded( function() {
-    initImages(d.e, $wr);
-
-    // Zoom in elements
-    sceneElementsZoomTransition();
-
-    // initOrbits();
-
+    var textElements = function() {
+      var $wr = $('#scene-elements');
+      initTextEl(d.e, $wr);
+      initTextEl(d.n, $wr);
+      sPreEtransition(sEtransition);
+    };
+    var imageElements = function(callback) {
+      var $wr = $('#scene-elements');
+      initImages(d.e, $wr);
+      // sPreEtransition(sEtransition);
+      callback();
+    };
+    imageElements(textElements);
+    setTimeout(function(){
+      initOrbits();
+    },5000);
   });
-
 });
 
 
@@ -156,28 +140,28 @@ $.fn.placeIn3dTargetZone = function(d) {
 /**
  *
  */
-function sPreEtransition() {
+var sPreEtransition = function(callback) {
   var $s = $('.scene-element');
   $s.each(function(i){
+    if ($s.attr('state') == 'done') return;
     var $e = $(this);
-    var $w = $('#grid-svg');
-    var x1 = $(window).width() / 2;
+    var x1 = ($(window).width() / 2) + ($e.width() / 2);
     var y1 = $(window).height() / 2 - ($(window).height() / 8);
     $e.css({
-      transform: 'translate(' + x1 + 'px, ' + y1 + 'px)',
+      transform: 'translate(' + x1 + 'px, ' + y1 + 'px) scale(0.01)',
     });
   });
-}
+  setTimeout(function(){
+    callback();
+  }, 300);
+};
 /**
  *
  */
-function sEtransition() {
+var sEtransition = function() {
   var $s = $('.scene-element');
   $s.each(function(i){
     var $e = $(this);
-    var $w = $('#grid-svg');
-    var x1 = $(window).width() / 2;
-    var y1 = $(window).height() / 2 - ($(window).height() / 8);
     var x2 = $e.attr('l');
     var y2 = $e.attr('t');
     setTimeout(function(){
@@ -185,89 +169,30 @@ function sEtransition() {
         translateX: x2,
         translateY: y2,
         opacity: 1,
-      });
-      // bounceInPlace($el, numberBetween(100,150));
-    }, 300 * i);
-  });
-}
-
-/**
- * Fade in Names
- */
-function sceneElementsFadeTransition() {
-  var $fadeElements = $('.scene_element--name');
-  $fadeElements.each(function(i){
-    var $el = $(this);
-    setTimeout(function(){
-      $el.addClass('se-visible');
-      // bounceInPlace($el, numberBetween(100,150));
-    }, 300 * i);
-  });
-}
-
-/**
- *
- */
-function sceneElementsZoomTransition() {
-  var $zoomElements = $('.scene-element__zoom-in');
-  setTimeout(function(){
-    $zoomElements.each(function(i){
-      var $e = $(this);
-      setTimeout(function(){
-        // $e.css({
-        //   transform: 'scale(' + ($e.attr('scale')) + ')'
-        // });
-        $e.addClass('se-visible');
-        $e.removeClass('scene-element__zoom-in');
-        if ($e.hasClass('text')) {
-          setTimeout(function(){
-            bounceInPlace($e, numberBetween(100,150));
-          }, 600);
+        scale : $e.attr('scale'),
+        tween: 1000,
+      },
+      {
+        progress: function(elements, complete, remaining, start, tweenValue) {
+          // console.log((complete * 100) + "%");
+          // console.log(remaining + "ms remaining!");
+          // console.log("The current tween value is " + tweenValue);
         }
-        if ($e.hasClass('image')) {
-          setTimeout(function(){
-            var tz = $e.attr('target-zone');
-            var $tz = $('[segment="' + parseInt(tz) + '"]');
-            var cur1 = $e.offset().left;
-            var pos1 = $tz.offset().left + $tz.width();
-            var pos2 = $tz.offset().left;
-
-            function bounceWallInit($e, $tz, i) {
-              // Calculate random point on $tz rectangle perimeter.
-              var s = numberBetween(1, 4);
-              var o = s%2 == 0 ? 'y' : 'x';
-              var pos = numberBetween(1, $tz.width());
-              var bounds = $e[0].getBoundingClientRect();
-
-              var x = $tz.width() - (bounds.width / 2 * i);
-              var y = $tz.height() - (bounds.height / 2 * i);
-
-              var properties = {
-                translateX: numberBetween(0, $tz.width()) - $tz.offset().left,
-                translateY: numberBetween(0, $tz.width()) - $tz.offset().top,
-                scale: [$e.attr('scale'), $e.attr('scale')]
-              };
-              var dur = 7000;
-              $e.velocity(
-                properties
-                , {
-                easing: 'linear',
-                duration: dur,
-                complete: function(){
-                  bounceWallInit($e, $tz, i);
-                }
-              });
-            }
-            setTimeout(function(){
-              bounceWallInit($e, $tz, 1);
-            }, 200 * i);
-
-          }, 1000);
-        }
-      }, 50 * i);
     });
-  }, 700);
-}
+      setTimeout(function(){
+        switch ($e.attr('type')) {
+          case 'text':
+           bounceInPlace($e, numberBetween(100,150));
+            break;
+          case 'image':
+
+            break;
+        }
+        $e.attr('state', 'done');
+      }, 500);
+    }, 300 * i);
+  });
+};
 
 /**
  *
@@ -275,13 +200,13 @@ function sceneElementsZoomTransition() {
 function bounceInPlace($e, dist) {
   // Up.
   $e.velocity({
-    translateY: dist * -1,
+    translateY: '+='+(dist * -1),
   }, {
     duration: numberBetween(200,500),
   }, "easeOutCirc");
   // Down.
   $e.velocity({
-    translateY: dist,
+    translateY: '+='+dist,
   }, {
     duration: numberBetween(4000, 5000),
     complete: function() {
@@ -295,51 +220,67 @@ function bounceInPlace($e, dist) {
  * Init orbits.
  */
 function initOrbits() {
-  for (var i = 0; i < sE.length; i++) {
-    var $el = $('#' + sE[i].name);
+  $('.scene-element.image').each(function(){
+    var $e = $(this);
+    var tz = $e.attr('target-zone');
+    var $tz = $('[segment="' + tz + '"]');
+    function orbit($e) {
+      var s = numberBetween(0,3);
 
-    var targetZone = sE[i].targetZone;
+      var o = s%2 ? 'y' : 'x';
+      var x1 = numberBetween(0, $tz.width());
+      var y1 = numberBetween(0, $tz.width());
+      var x = 0;
+      var y = 0;
+      var offset = $tz.offset();
+      x = offset.left + x1 - $e.offset().left;
+      y = offset.top + y1 - $e.offset().top;
+      // switch (s) {
+      //   case 0:
+      //     x = offset.left + x1 - $e.offset().left;
+      //     break;
+      //   case 1:
+      //     x = offset.left + $e.width();
+      //     y = offset.top + y1;
+      //     break;
+      // }
+      setTimeout(function(){
+        $e.velocity({
+          translateX: '+=' + x,
+          translateY: '+=' + y,
+        }, {
+          'duration': numberBetween(8888,11111),
+          'easing': 'linear',
+        }, {
+          complete: function() {
+            orbit($e);
+          }
+        });
+        $e.velocity('reverse', {
+          complete: function(){
+            orbit($e);
+          }
+        });
 
-    var speed = 10;
-    // var targetZoneId = (window.innerWidth < 600) ? '#scene-elements' : '#grid-' + targetZone;
-    var targetZoneId = '#grid-' + targetZone;
-    if ($el.attr('type') == 'image') {
-      $el.bounce('start', {
-        'minSpeed'	: speed,
-        'maxSpeed'	: speed,
-        'zone'		: targetZoneId
       });
-      $el.hover(function() {
-        $(this).each(function() {
-          $(this).bounce('stop');
-        });
-      }, function() {
-        $(this).each(function() {
-          $(this).bounce('start', {
-            'minSpeed'	: speed,
-            'maxSpeed'	: speed,
-            'zone'		: targetZoneId
-          });
-        });
-      });
-
     }
-  }
-  $('.master-controls .close').click(function(){
-    for (var i = 0; i < sE.length; i++) {
-      var $el = $('#' + sE[i].name);
-      var targetZone = sE[i].targetZone;
-      var speed = 10;
-      var targetZoneId = (window.innerWidth < 600) ? '#scene-elements' : '#grid-' + targetZone;
-      if ($el.attr('type') == 'image') {
-        $el.bounce('start', {
-          'minSpeed'	: speed,
-          'maxSpeed'	: speed,
-          'zone'		: targetZoneId
-        });
-      }
-    }
+    orbit($e);
   });
+  // $('.master-controls .close').click(function(){
+  //   for (var i = 0; i < sE.length; i++) {
+  //     var $el = $('#' + sE[i].name);
+  //     var targetZone = sE[i].targetZone;
+  //     var speed = 10;
+  //     var targetZoneId = (window.innerWidth < 600) ? '#scene-elements' : '#grid-' + targetZone;
+  //     if ($el.attr('type') == 'image') {
+  //       $el.bounce('start', {
+  //         'minSpeed'	: speed,
+  //         'maxSpeed'	: speed,
+  //         'zone'		: targetZoneId
+  //       });
+  //     }
+  //   }
+  // });
 }
 
 function thumbnailSize() {
